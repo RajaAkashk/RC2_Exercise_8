f40f1531089385c61796dedfaa860e0410eb1090
Raja Akash Kumar <159010538+RajaAkashk@users.noreply.github.com>
2024-06-18 16:23:58 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+
+<body>
+
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand fs-4 fw-normal" href="index.html">Computer Management</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link  fs-5 " aria-current="page" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fs-5 " href="compuerList.html">Computer List</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link fs-5 " href="addComputer.html">Add Computers</a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+
+  <section class="pt-5 mt-3">
+    <div class="container">
+
+      <div class="text-center py-5 mt-4 bg-primary rounded ">
+        <h1 class="pt-4 display-4 fw-normal text-light">Welcome to Computer Management</h1>
+        <p class="pt-2 pb-4 fs-5 fw-normal text-light">Manage Computer record with this simple management system.</p>
+      </div>
+
+
+      <div class="text-center mt-5 pt-4">
+        <h2 class="display-7 fw-medium">Computer List</h2>
+        <p class="fw-normal fs-5">View and manage Computers in your system.</p>
+        <a href="compuerList.html" class="btn btn-primary fs-5">Computer List</a>
+      </div>
+
+
+      <div class="text-center mt-5 pt-4">
+        <h2 class="display-7 fw-medium">Manage Computers</h2>
+        <p class="fw-normal fs-5">Add a new Computer to the system with its details. </p>
+        <a href="addComputer.html" class="btn btn-primary fs-5">Add Computer</a>
+      </div>
+
+    </div>
+  </section>
+
+  <footer class="mt-5">
+    <div class="text-center">
+      <p class="fs-5 fw-normal">&copy;2024 Computer Management. All rights reserved. </p>
+    </div>
+  </footer>
+
+
+
+  <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
